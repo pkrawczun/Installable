50cd9d5c731f9f3fd06eefdbb8b6d67ced6ba68b
pkrawczun <pkrawczun1@gmail.com>
2025-02-27 15:58:51 +0100

    Initial commit

---
+++ build/Desktop-Release/CMakeFiles/Installable.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Installable_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Installable_autogen.dir/ParseCache.txt"
+  "Installable_autogen"
+  "CMakeFiles/Installable.dir/Installable_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Installable.dir/Installable_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Installable.dir/main.cpp.o"
+  "CMakeFiles/Installable.dir/main.cpp.o.d"
+  "CMakeFiles/Installable.dir/mainwindow.cpp.o"
+  "CMakeFiles/Installable.dir/mainwindow.cpp.o.d"
+  "Installable"
+  "Installable.pdb"
+  "Installable_autogen/mocs_compilation.cpp"
+  "Installable_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Installable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
